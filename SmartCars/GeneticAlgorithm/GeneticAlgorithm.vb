23ae7c9fa0fc598be38f47b19a34763a62d98474
knappyflash <knappyflash2@gmail.com>
2025-11-23 08:48:40 -0800

    a

--- SmartCars/GeneticAlgorithm/GeneticAlgorithm.vb
+++ SmartCars/GeneticAlgorithm/GeneticAlgorithm.vb
@@ -1,25 +1,20 @@
 ﻿Public Class GeneticAlgorithm
     Public NeuralNetworks As New List(Of NeuralNetwork)
-    Public inputs() As Double
-    Public outputs() As Double
     Public Sub New(
                   populationSize As Integer,
                   inputCount As Integer,
                   outputCount As Integer,
                   hiddenLayerCount As Integer,
                   NeuronsPerHiddenLayer As Integer)
-        ReDim inputs(inputCount - 1)
-        ReDim outputs(outputCount - 1)
         For i = 0 To populationSize - 1
             NeuralNetworks.Add(New NeuralNetwork(inputCount, outputCount, hiddenLayerCount, NeuronsPerHiddenLayer))
         Next
     End Sub
 
-    Public Sub Iterate()
+    Public Sub NextGeneration()
         SortNeuralNetworksByFitness()
         KillBadPerformers()
         PerformCrossover()
-        RunGeneration()
     End Sub
 
     Public Sub SortNeuralNetworksByFitness()
@@ -61,12 +56,6 @@
     Public Sub RandomizeSome()
         For i As Integer = 95 To 99
             Me.NeuralNetworks(i).Randomize()
-        Next
-    End Sub
-
-    Public Sub RunGeneration()
-        For i As Integer = 0 To NeuralNetworks.Count - 1
-            outputs = NeuralNetworks(i).PropogateForward(inputs)
         Next
     End Sub
 
--- SmartCars/QuickTest.vb
+++ SmartCars/QuickTest.vb
@@ -10,10 +10,29 @@
 "
     End Sub
     Private Sub QuickTestButton_Click(sender As Object, e As EventArgs) Handles QuickTestButton.Click
-        Me.geneticAlgorithm.inputs(0) = CDbl(Me.TextBox1.Text)
-        Me.geneticAlgorithm.inputs(1) = CDbl(Me.TextBox2.Text)
-        Me.geneticAlgorithm.Iterate()
-        Me.Label2.Text = CInt(Me.geneticAlgorithm.outputs(0))
+        Dim v1 As Double = CDbl(Me.TextBox1.Text)
+        Dim v2 As Double = CDbl(Me.TextBox2.Text)
+        Dim inputs() As Double = {v1, CDbl(v2)}
+
+        For i As Integer = 0 To Me.geneticAlgorithm.NeuralNetworks.Count - 1
+            Dim score As Double = 0
+            Dim outputs() As Double = Me.geneticAlgorithm.NeuralNetworks(i).PropogateForward(inputs)
+
+            If i = 0 Then
+
+                Me.Label2.Text = CInt(outputs(0))
+            End If
+
+            If (v1 = 0) And (v2 = 0) And (CInt(outputs(0)) = 0) Then score = score + 1
+            If (v1 = 0) And (v2 = 1) And (CInt(outputs(0)) = 1) Then score = score + 1
+            If (v1 = 1) And (v2 = 0) And (CInt(outputs(0)) = 1) Then score = score + 1
+            If (v1 = 0) And (v2 = 0) And (CInt(outputs(0)) = 0) Then score = score + 1
+            Me.geneticAlgorithm.NeuralNetworks(i).FitnessScore = score
+            Debug.WriteLine($"{v1} {v2} {CInt(outputs(0))} {score}")
+        Next
+
+        Me.geneticAlgorithm.NextGeneration()
+
     End Sub
 
 End Class
