2160687d9ec783cc615c5078bb360401be5b6678
knappyflash <knappyflash2@gmail.com>
2025-12-15 12:19:37 -0800

    a

--- SmartCars/SmartCars/SmartCars.vb
+++ SmartCars/SmartCars/SmartCars.vb
@@ -50,7 +50,6 @@
                     Me.Cars(i).ShouldBeHeading = Car.CorrectDirecton.east
                 End If
             Next
-            'test
             For Each tile As TrackTile In TrackMap.HeadSouthTiles
                 If (Me.Cars(i).posX > tile.Rect.X) And
                         (Me.Cars(i).posX < tile.Rect.X + tile.Rect.Width) And
